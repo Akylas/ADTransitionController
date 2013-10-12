7bb8916999d328550485474b9accf9edfbc1316c
Martin Guillon <martin.guillon@gmail.com>
2013-10-12 15:06:09 +0200

    better ghost animation

--- ADTransitionController/ADGhostTransition.m
+++ ADTransitionController/ADGhostTransition.m
@@ -14,29 +14,38 @@
     CABasicAnimation * inFadeAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
     inFadeAnimation.fromValue = @0.0f;
     inFadeAnimation.toValue = @1.0f;
+    inFadeAnimation.duration = duration/2;
     
     CABasicAnimation * inScaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform"];
     inScaleAnimation.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(2.0f, 2.0f, 2.0f)];
     inScaleAnimation.toValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
+    inScaleAnimation.duration = duration;
+    inScaleAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
     
     CAAnimationGroup * inAnimation = [CAAnimationGroup animation];
     [inAnimation setAnimations:@[inFadeAnimation, inScaleAnimation]];
     inAnimation.duration = duration;
     
+    CABasicAnimation * outPositionAnimation = [CABasicAnimation animationWithKeyPath:@"zPosition"];
+    outPositionAnimation.fromValue = @-0.001;
+    outPositionAnimation.toValue = @-0.001;
+    outPositionAnimation.duration = duration;
+    
     CABasicAnimation * outFadeAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
     outFadeAnimation.fromValue = @1.0f;
     outFadeAnimation.toValue = @0.0f;
+    outFadeAnimation.duration = duration/2;
     
     CABasicAnimation * outScaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform"];
     outScaleAnimation.fromValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
     outScaleAnimation.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.01f, 0.01f, 0.01f)];
-    
+    outScaleAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+   
     CAAnimationGroup * outAnimation = [CAAnimationGroup animation];
-    [outAnimation setAnimations:@[outFadeAnimation, outScaleAnimation]];
+    [outAnimation setAnimations:@[outFadeAnimation, outScaleAnimation, outPositionAnimation]];
     outAnimation.duration = duration;
     
     self = [super initWithInAnimation:inAnimation andOutAnimation:outAnimation];
     return self;
 }
-
 @end
